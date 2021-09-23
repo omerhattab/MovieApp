8c22c5097551a3f2e0aecbd2f753b5e6b5c235dd
Ömer <omerhattabsaitoglu@gmail.com>
2021-09-23 18:58:02 +0300

    Son İstenilen Eksik Kalan Değişiklikler

--- moviesApp/app/app.js
+++ moviesApp/app/app.js
@@ -2,41 +2,21 @@ var movieApp = angular.module('movieApp', ['ngRoute']);
 
 //Router
 movieApp.config(function ($routeProvider) {
-    const pages = {
-        home: {
-            url: '/',
-            templateUrl: '/views/home/home.html',
-            controller: 'movieController'
-        },
-        films: {
-            url: '/',
-            templateUrl: '/views/movies/films.html',
-            controller: 'movieController'
-        },
-        series: {
-            url: '/',
-            templateUrl: '/views/movies/series.html',
-            controller: 'movieController'
-        },
-        error: {
-            url: '/',
-            templateUrl: '/views/error/error.html',
-            controller: 'movieController'
-        }
-    }
-
     //Router Url
     $routeProvider.when('/', {
         title: 'Popular Titles',
-        templateUrl: pages.home.templateUrl
+        templateUrl: '/views/home/home.html',
+        controller: 'movieController'
     }).when('/Films', {
         title: 'Popular Films',
-        templateUrl: pages.films.templateUrl
+        templateUrl: '/views/movies/films.html',
+        controller: 'movieController'
     }).when('/Series', {
         title: 'Popular Series',
-        templateUrl: pages.series.templateUrl
+        templateUrl: '/views/movies/series.html',
+        controller: 'movieController'
     }).otherwise({
-        templateUrl: pages.error.templateUrl
+        templateUrl: '/'
     });
 });
 
@@ -48,7 +28,6 @@ movieApp.run(function ($rootScope) {
 
 movieApp.controller('movieController', function ($scope) {
     $scope.copyDate = new Date();
-
 });
 
 
--- moviesApp/app/controller/movies.js
+++ moviesApp/app/controller/movies.js
@@ -1,22 +1,42 @@
 movieApp.controller('moviesController', function ($scope, $http) {
+    //Loading
+    $scope.loading = true;
+    $scope.loadMovies = false;
+
     //Json Data
     $http.get('app/model/sample.json').then(function (response) {
         const moviesResponse = response.data.entries;
         $scope.movies = moviesResponse;
+        $scope.errorAlert = false; 
+        $scope.loadMovies = true;       
+    }).catch(function(error){
+        if(error.status == '404'){
+            $scope.errorAlert = true;
+            $scope.loading = false;
+        }
+    }).finally(function(){
+        $scope.loading = false;
+        $scope.loadMovies = true; 
     });
 
     //Page Number Limit
     var page = 1;
-    var countItem = 11;
+    var countItem = 21;
     $scope.pageLimit = function (data) {
         return countItem * page;
     };
 
     //More Upload 
     $scope.movieMore = function () {
-        page = page + 1;  
+        page = page + 1; 
     }
     
+    //Filter Year => 2010
+    $scope.filterYear = function (item) {
+        if(item.releaseYear >= 2010){
+            return item;
+        }
+    };
 
     //Text Three Dots
     $scope.add3Dots = function (string, limit) {
